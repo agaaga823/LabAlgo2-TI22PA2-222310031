1cf336b003fd5b03f3f506dd038125d25a3fed05
agaaga823 <113017632+agaaga823@users.noreply.github.com>
2023-07-01 08:43:23 +0700

    Update Tugas 5 no 1-3.cpp

--- Tugas 5/Tugas 5 no 1-3.cpp
+++ Tugas 5/Tugas 5 no 1-3.cpp
@@ -2,51 +2,74 @@
 using namespace std;
 
 struct Node {
-    int data;
-    Node* left;
-    Node* right;
+  int data;
+  Node *left;
+  Node *right;
 };
 
-Node* createNode(int data) {
-    Node* newNode = new Node();
-    if (!newNode) {
-        cout << "Gagal membuat node baru." << endl;
-        return NULL;
-    }
-    newNode->data = data;
-    newNode->left = newNode->right = NULL;
-    return newNode;
+Node *createNode(int data) {
+  Node *newNode = new Node();
+  if (!newNode) {
+    cout << "Gagal membuat node baru." << endl;
+    return NULL;
+  }
+  newNode->data = data;
+  newNode->left = newNode->right = NULL;
+  return newNode;
 }
 
-Node* insertNode(Node* root, int data){
-    if (root == NULL){
-        root = createNode(data);
-        return root;
-    }
-    if (data < root->data)
-        root->left = insertNode(root->left, data);
-    else if (data >= root->data)
-        root->right = insertNode(root->right, data);
+Node *insertNode(Node *root, int data) {
+  if (root == NULL) {
+    root = createNode(data);
     return root;
+  }
+  if (data < root->data)
+    root->left = insertNode(root->left, data);
+  else if (data >= root->data)
+    root->right = insertNode(root->right, data);
+  return root;
 }
 
-void preOrderTraversal(Node* root) {
-    if (root == NULL)
-        return;
+void preOrderTraversal(Node *root) {
+  if (root == NULL)
+    return;
 
-    cout << root->data << " "; 
-    preOrderTraversal(root->left);
-    preOrderTraversal(root->right);
+  cout << root->data << " ";
+  preOrderTraversal(root->left);
+  preOrderTraversal(root->right);
+}
+void inOrderTraversal(Node *root) {
+  if (root == NULL)
+    return;
+
+  inOrderTraversal(root->left);
+  cout << root->data << " ";
+  inOrderTraversal(root->right);
+}
+void postOrderTraversal(Node *root) {
+  if (root == NULL)
+    return;
+
+  postOrderTraversal(root->left);
+  postOrderTraversal(root->right);
+  cout << root->data << " ";
 }
 
 int main() {
-    Node* root = NULL;
-    root = insertNode(root, 12);
-    root = insertNode(root, 16);
-    root = insertNode(root, 20);
-    root = insertNode(root, 24);
-    root = insertNode(root, 32);
-    cout << "Hasil pre-order traversal: ";
-    preOrderTraversal(root);
-    return 0;
+  Node *root = NULL;
+  root = insertNode(root, 12);
+  root = insertNode(root, 16);
+  root = insertNode(root, 20);
+  root = insertNode(root, 24);
+  root = insertNode(root, 32);
+  cout << "Hasil pre-order traversal: ";
+  preOrderTraversal(root);
+  cout << endl;
+  cout << "Hasil in-order traversal: ";
+  inOrderTraversal(root);
+  cout << endl;
+  cout << "Hasil post-order traversal: ";
+  postOrderTraversal(root);
+  cout << endl;
+  return 0;
 }
